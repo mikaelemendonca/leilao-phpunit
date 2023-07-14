e7fdbf03642f12c07d70c641aed6cbb718413bce
Mikaele Costa <mikaelemendonca@gmail.com>
2023-07-13 22:52:37 -0300

    Testes intermediários

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -96,16 +96,21 @@ class LeilaoDaoTest extends TestCase
 
     public function testAoAtualizarLeilaoStatusDeveSerAtualizado()
     {
-        // arrage
         $leilao = new Leilao('Brasilia Amarela');
         $leilaoDao = new LeilaoDao(self::$pdo);
         $leilao = $leilaoDao->salva($leilao);
-        $leilao->finaliza();
 
-        // act
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+        self::assertCount(1, $leiloes);
+        self::assertSame(
+            'Brasilia Amarela',
+            $leiloes[0]->recuperarDescricao()
+        );
+        self::assertFalse($leiloes[0]->estaFinalizado());
+
+        $leilao->finaliza();
         $leilaoDao->atualiza($leilao);
 
-        // assert
         $leiloes = $leilaoDao->recuperarFinalizados();
         self::assertCount(1, $leiloes);
         self::assertSame(
